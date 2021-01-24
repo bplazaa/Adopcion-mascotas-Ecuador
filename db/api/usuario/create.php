0952668d7c44a0cee3e208b875ea70cca422a9d8
VictorAlvaradoAraujo <viosalva@espol.edu.ec>
2021-01-24 17:15:15 -0500

    crud de usuario

---
+++ db/api/usuario/create.php
@@ -0,0 +1,71 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/mascota.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+$mascota = new Mascota($db);
+
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+  
+// make sure data is not empty
+if(
+        !empty($data->nombre) &&
+        !empty($data->raza) &&
+        !empty($data->contacto) &&
+        !empty($data->sexo)&&
+        !empty($data->especie)&&
+        !empty($data->foto)
+){
+
+        // set product property values
+        $mascota->nombre = $data->nombre;
+        $mascota->raza = $data->raza;
+        $mascota->contacto = $data->contacto;
+        $mascota->sexo = $data->sexo;
+        $mascota->especie = $data->especie;
+        $mascota->foto = $data->foto;
+    
+        // create the product
+        if($mascota->create()){
+    
+            // set response code - 201 created
+            http_response_code(201);
+    
+            // tell the user
+            echo json_encode(array("mensaje" => "La macota fue creada."));
+        }
+    
+        // if unable to create the product, tell the user
+        else{
+    
+            // set response code - 503 service unavailable
+            http_response_code(503);
+    
+            // tell the user
+            echo json_encode(array("mensaje" => "No se puede crear la mascota."));
+        }
+    }
+    
+    // tell the user data is incomplete
+    else{
+    
+        // set response code - 400 bad request
+        http_response_code(400);
+    
+        // tell the user
+        echo json_encode(array("mensaje" => "No se puede crear la mascota. Datos incompletos."));
+    }
+?>
---
+++ db/api/usuario/read.php
@@ -0,0 +1,52 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+  
+
+include_once '../config/database.php';
+include_once '../objects/mascota.php';
+  
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+$mascota = new Mascota($db);
+
+$stmt = $mascota->read();
+$num = $stmt->rowCount();
+  
+// Si se han encontrado mas de 0 resultados de mascota
+if($num>0){
+  
+    // products array
+    $mascotas_arr=array();
+    $mascotas_arr["records"]=array();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+  
+        $mascota_item=array(
+            "nombre" => $nombre,
+            "raza" => $raza,
+            "contacto" => $contacto,
+            "sexo" => $sexo,
+            "especie" => $especie
+        );
+  
+        array_push($mascotas_arr["records"], $mascota_item);
+    }
+  
+
+    http_response_code(200); // OK
+    echo json_encode($mascotas_arr); // Envia datos como json
+}
+  
+else{
+    // No se han encontrado mascotas
+    http_response_code(404); // Error
+    echo json_encode(
+        array("mensaje" => "No se encontro a la mascota")
+    );
+}
+
