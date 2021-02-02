de45109df39eda5490af03bd51bd85752dae5703
paulabenites <paula.beni.tes@hotmail.com>
2021-02-01 22:44:54 -0500

    Contactanos interfaz

--- Adopcionmascotas/app/src/main/java/com/example/adopcionmascotas/ui/contactanos/ContactanosFragment.kt
+++ Adopcionmascotas/app/src/main/java/com/example/adopcionmascotas/ui/contactanos/ContactanosFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.adopcionmascotas.R
+import com.squareup.picasso.Picasso
 
 class ContactanosFragment : Fragment() {
 
@@ -27,6 +29,9 @@ class ContactanosFragment : Fragment() {
         contactanosViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val imageContactos = root.findViewById<ImageView>(R.id.imageViewContactanos)
+        Picasso.get().load("https://media.metrolatam.com/2020/06/10/perros1024x574-17bc3b2637843af290af3eda2cfbd2cb-1200x600.jpg").into(imageContactos)
 
 //        val map:WebView = root.findViewById(R.id.webMap)
 //        map.loadUrl("https://goo.gl/maps/z14bPZsv5z5CVHvj7")
